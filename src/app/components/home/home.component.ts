b15896eb04c7b14da711064b823318ffb4578563
Vytautas <vmizgiris@hotmail.com>
2021-10-24 09:03:41 +0800

    feat: responsiveness for quizcrt, home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { BaseQuiz } from 'src/app/models/quiz';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent { }
+export class HomeComponent implements OnInit, OnDestroy {
+  inProgress: Omit<BaseQuiz, 'options'>[] = [];
+  latest: Omit<BaseQuiz, 'options'>[] = [];
+  currentDate = new Date();
+  $clockSubscription?: Subscription;
+
+  ngOnInit() {
+    this.$clockSubscription = interval(1000).pipe(
+      tap(() => this.currentDate = new Date())
+    ).subscribe();
+  }
+
+  ngOnDestroy() {
+    this.$clockSubscription?.unsubscribe();
+  }
+}
