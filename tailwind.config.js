e2248aa927faa76367bf9dadfbb91345ec1338b6
Vytautas <vmizgiris@hotmail.com>
2022-10-23 18:19:27 +0800

    chore: update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,7 @@
 module.exports = {
-  // @see https://tailwindcss.com/docs/upcoming-changes
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
-  purge: ['./src/components/**/*.js', './pages/**/*.js'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/**/*.{html,ts}'],
   theme: {
     extend: {},
   },
-  variants: {},
-  plugins: [require('tailwindcss'), require('precss'), require('autoprefixer')],
+  plugins: [],
 };
