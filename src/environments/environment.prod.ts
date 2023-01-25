edd5cdd9b22bdf1ec1ffcb90ae8e80f0de94b0eb
Vytautas <vmizgiris@hotmail.com>
2023-01-25 19:25:57 +0800

    Update prod API url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   authClientId: 'ZS7gcZILlBprt4xxI3Vv2eSUzD4wupTE',
   authDomain: 'quizmkr.eu.auth0.com',
-  apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://api.quizmkr.com',
 };
