b708fb23a5b2ee6036aab35de306f5dd75c60e98
Vytautas Mizgiris <vmizgiris@hotmail.com>
2023-08-13 16:08:42 +0800

    Load Shoelace via npm

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path';
+import '@shoelace-style/shoelace';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+setBasePath('https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.7.0/cdn/');
 
 if (environment.production) {
   enableProdMode();
